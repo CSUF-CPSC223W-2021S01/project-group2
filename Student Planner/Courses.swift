dd75694a9565f2e565ac03165f8574119c3f09a8
Kaylee Bliaya <kbliaya@csu.fullerton.edu>
2021-03-23 10:51:18 -0700

    Finished Courses.swift (addCourse still needs more work) and started on CoursesTests.swift

--- Student Planner/Courses.swift
+++ Student Planner/Courses.swift
@@ -1,9 +1,39 @@
 import Foundation
 
 struct Courses {
-    var courseName: String // Computer Science 223W
-    var days: String // Tuesday & Thursday
-    var times: String // 9:00 pm - 10:50 pm
-    var courseProfessor: String // Paul Inventado
-    var weightType: String // Weighted
+    var name: String? // Computer Science 223W
+    var semesterName: String? // Spring 2021
+    var days: String? // Tuesday & Thursday
+    var times: String? // 9:00 pm - 10:50 pm
+    var courseProfessor: String? // Paul Inventado
+    var weightType: String? // Weighted
+
+    init?(name: String?, semesterName: String?, days: String?, times: String?, courseProfessor: String?, weightType: String?) {
+        // if the course name, semester name, or weight type are not given, then return nil
+        if name == nil || semesterName == nil || weightType == nil {
+            return nil
+        }
+        self.name = name
+        self.semesterName = semesterName
+        self.days = days
+        self.times = times
+        self.courseProfessor = courseProfessor
+        self.weightType = weightType
+    }
+}
+
+// arrays of Courses instances
+var courseArray: [Courses] = []
+// dictionary with key: semesterName & value: courseArray
+var semesterCourses = [String: [Courses]]()
+
+// function to add a new course to a semester or create a new semester
+func addCourse(semesterName: String, course: [Courses]) {
+    // if the semester given already exists then append a new Courses instance to courseArray and assign it to its key (semesterName); otherwise create a new key
+    if semesterCourses[semesterName] != nil {
+        courseArray.append(contentsOf: course)
+        semesterCourses[semesterName] = courseArray
+    } else {
+        semesterCourses[semesterName] = course
+    }
 }
--- Student PlannerTests/CoursesTests.swift
+++ Student PlannerTests/CoursesTests.swift
@@ -1,1 +1,37 @@
-import Foundation
+import XCTest
+@ testable import Student_Planner
+
+class StudentPlannerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    /*
+    var name: String? // Computer Science 223W
+    var semesterName: String? // Spring 2021
+    var days: String? // Tuesday & Thursday
+    var times: String? // 9:00 am - 10:50 am
+    var courseProfessor: String? // Paul Inventado
+    var weightType: String? // Weighted
+     
+     init?(name: String?, semesterName: String?, days: String?, times: String?, courseProfessor: String?, weightType: String?) 
+    */
+    func testNoName() throws {
+        let content = Courses(name: nil, semesterName: "Spring 2021", days: "Tuesday & Thursday", times: "9:00 am - 10:50 am", courseProfessor: "Paul Inventado", weightType: "Weighted")
+        XCTAssertNil(content)
+    }
+    
+    func testNoSemesterName() throws {
+        let content = Courses(name: "CPSC 223W", semesterName: nil, days: "Tuesday & Thursday", times: "9:00 am - 10:50 am", courseProfessor: "Paul Inventado", weightType: "Weighted")
+        XCTAssertNil(content)
+    }
+    
+    func testNoWeightType() throws {
+        let content = Courses(name: "CPSC 223W", semesterName: "Spring 2021", days: "Tuesday & Thursday", times: "9:00 am - 10:50 am", courseProfessor: "Paul Inventado", weightType: nil)
+        XCTAssertNil(content)
+    }
+}
