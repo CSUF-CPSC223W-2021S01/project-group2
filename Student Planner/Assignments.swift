cb4e244ca2f0853b69bda4845252d3010ee0c049
Wesley Chou <wesleychou@csu.fullerton.edu>
2021-03-24 13:55:31 -0700

    added enum and nil

--- Student Planner/Assignments.swift
+++ Student Planner/Assignments.swift
@@ -1,20 +1,31 @@
+//Wesley Chou
+//Last Date of Update: 3/24/2021
 import Foundation
-class Assignments{
-    var assignment : [String: String]   // dictionary, key: name value: due date/ time
-    var totalAssignments: Int           // computed Property
-    init(){
-        self.assignment = [:]
-        self.totalAssignments = 0
+enum AssignmentType{
+    case homework
+    case quiz
+    case exam
+    case extraCredit
+    case project 
+}
+class Assignments {
+    var assignment: [String: String]?
+    var totalAssignments: Int?
+    init?(assignment: [String: String]!, totalAssignments: Int!) {
+        if assignment == nil, totalAssignments == nil {
+            return nil
+        }
+        else {
+            self.assignment = [:]
+            self.totalAssignments = 0
+        }
     }
-    func add(_ name: String, due: String){
-        totalAssignments = totalAssignments + 1
-        
+
+    func add(_ name: String, due: String) {
+        totalAssignments = totalAssignments! + 1
     }
-    func completedAssignment(){
-        totalAssignments = totalAssignments - 1
+
+    func completedAssignment() {
+        totalAssignments = totalAssignments! - 1
     }
 }
-// Things to do:
-    // Figure out how to get a time as a String, or do we need a different variable type
-    // Refer to Courses to choose the class that we are adding an Assignment into
-    // update completedAssignment when button actions are avaliable
