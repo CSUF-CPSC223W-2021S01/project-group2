b68d28e009af30cf277a4db526846863a33e4abb
Kaylee Bliaya <kbliaya@csu.fullerton.edu>
2021-04-22 10:03:34 -0700

    made changes to Courses.swift and added an icon to the Courses page on the Main.storyboard

--- Student Planner/Courses.swift
+++ Student Planner/Courses.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-struct Courses {
+struct Courses: Codable, Equatable {
     var name: String // Computer Science 223W
     var semester: String // Spring 2021
     var weightType: String // Weighted
@@ -9,33 +9,50 @@ struct Courses {
     var courseProfessor: String? // Paul Inventado
     var semesterCourses = [Courses]()
 
+    // protocol to check if two Courses objects are equal
+    static func == (lhs: Courses, rhs: Courses) -> Bool {
+        return lhs.name == rhs.name && lhs.semester == rhs.semester && lhs.weightType == rhs.weightType && lhs.days == rhs.days && lhs.times == rhs.times && lhs.courseProfessor == rhs.courseProfessor
+    }
+
     mutating func addCourse(newCourse: Courses) -> Bool {
-        // if the given Courses instance has a matching name and semester in the array return false, otherwise add the newCourse and return true
+        // if the given Courses object newCourse and a Courses object in the array match return false, otherwise append newCourse to the array and return true
         for course in semesterCourses {
-            if course.name == newCourse.name, course.semester == newCourse.semester {
+            if course == newCourse {
                 return false
             }
         }
         semesterCourses.append(newCourse)
         return true
     }
+
+    mutating func removeCourse(courseToBeRemoved: Courses) -> Bool {
+        // if the given Courses object newCourse and a Courses object in the array match remove it from the array and return true, otherwise return false
+        var arrayIndex = 0
+        for course in semesterCourses {
+            if course == courseToBeRemoved {
+                semesterCourses.remove(at: arrayIndex)
+                return true
+            }
+            arrayIndex += 1
+        }
+        return false
+    }
 }
 
-/*
- let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
- let archiveURL = documentsDirectory.appendingPathComponent("courses_array").appendingPathExtension("plist")
+let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+let archiveURL = documentsDirectory.appendingPathComponent("Courses").appendingPathExtension("plist")
 
- func encodeCourseArray() {
-     let propertyListEncoder = PropertyListEncoder()
-     let encodedCourses = try? propertyListEncoder.encode(semesterCourses)
-     try? encodedCourses?.write(to: archiveURL, options: .noFileProtection)
- }
+func encodeCourseArray(course: Courses) {
+    let propertyListEncoder = PropertyListEncoder()
+    let encodedCourses = try? propertyListEncoder.encode(course.semesterCourses)
+    try? encodedCourses?.write(to: archiveURL, options: .noFileProtection)
+}
 
- func decodeCourseData() {
-     let propertyListDecoder = PropertyListDecoder()
-     if let retrievedCoursesData = try? Data(contentsOf: archiveURL),
-         let decodedCourses = try? propertyListDecoder.decode(Array<Courses>.self, from: retrievedCoursesData) {
-         print(decodedCourses)
-     }
- }
- */
+func decodeCourseData() {
+    let propertyListDecoder = PropertyListDecoder()
+    if let retrievedCoursesData = try? Data(contentsOf: archiveURL),
+       let decodedCourses = try? propertyListDecoder.decode([Courses].self, from: retrievedCoursesData)
+    {
+        print(decodedCourses)
+    }
+}
