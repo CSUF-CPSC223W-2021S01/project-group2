4f50b3863ad636a6ce71ac69fcd4400915c66730
Kaylee Bliaya <54484110+Kbliaya@users.noreply.github.com>
2021-05-11 11:36:42 -0700

    Delete DetailsViewController.swift

--- Student Planner/Courses/DetailsViewController.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-class DetailsViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-}
