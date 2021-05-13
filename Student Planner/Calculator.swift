009e4eea42a8163c2e3ba72e32df30642f783cd4
Salam Abdelhalim <abed.pali@csu.fullerton.edu>
2021-05-12 22:59:45 -0700

    calculator file

--- Student Planner/Calculator.swift
+++ Student Planner/Calculator.swift
@@ -1,10 +1,34 @@
 import Foundation
-
-struct Grades {
-    var assignments = [Double:Double]()
-    var classTotal: Double?
-    //var expected: Double?
-    
-    
+class Grades {
+    var gradeRecieved: [String: Int] // dictionary with assignment name and grade recieved
+    var gradePossible: [String: Int] // dictionary with assignment name and grade possible
+    var recievedPoints: Int
+    var possiblePoints: Int
+    var grade: Int
    
+    init?() {
+        gradeRecieved = [:]
+        gradePossible = [:]
+        recievedPoints = 0
+        possiblePoints = 0
+        grade = 0
+    }
+
+    func add(name: String, gradeRec: Int, gradePos: Int ) {
+        gradeRecieved[name] = gradeRec
+        gradePossible[name] = gradePos
+    
+    }
+    
+    func calcGrade() {
+        for recValue in gradeRecieved.values {
+            recievedPoints += recValue
+        }
+        for posValue in gradePossible.values {
+            possiblePoints += posValue
+        }
+        grade = (recievedPoints/possiblePoints) * 100
+    }
 }
+
+
