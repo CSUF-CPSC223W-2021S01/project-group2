e00ba1b08318136bf74599d01a725e85790c4f75
Kaylee Bliaya <54484110+Kbliaya@users.noreply.github.com>
2021-05-13 01:48:33 -0700

    Revert "professor files and story board"

--- Student Planner/ViewController.swift
+++ Student Planner/ViewController.swift
@@ -4,16 +4,14 @@
 //
 //  Created by Kaylee Bliaya on 3/1/21.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
-    // Assignment Section----------------
-    // By Wesley Chou
-    @IBOutlet var assignName: UITextField!
-    @IBOutlet var DueDate: UITextField!
-    @IBOutlet var count: UITextField!
-    @IBOutlet var list: UITextView!
+
+     // Assignment Section----------------
+    @IBOutlet var DueDate: UIDatePicker!
+    @IBOutlet var AssignmentName: UITextField!
+    @IBOutlet var assignCount: UITextField!
     var assign = Assignments()
     // End Assigment Section ----------------
     override func viewDidLoad() {
@@ -21,17 +19,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    // Assignment Section --------
-    @IBAction func addAssignment(_ sender: UIButton) {
-        let assignmentName = String(assignName.text!)
-        let dueDate = String(DueDate.text!)
-        assign?.add(name: assignmentName, due: dueDate)
-        DueDate?.text = ""
-        assignName?.text = ""
-        print(assign!.printCount)
-        count.text = assign?.printCount
-        list.text = assign?.printDictionary
+    // Assignment Section----------------
+    @IBAction func addAssignment(_ sender: Any) {
+        let assignmentName = String(AssignmentName.text!)
+        assign?.add(assignmentName, due: DueDate.date)
+        
+        print(assign!.printOut)
+        assignCount.text = assign?.printOut
     }
-
     // End Assigment Section ----------------
 }
