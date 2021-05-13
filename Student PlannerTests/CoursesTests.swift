1a6825e0e00fd400545e19e8ee77a97b6dce6684
Kaylee Bliaya <kbliaya@csu.fullerton.edu>
2021-05-12 22:14:11 -0700

    updated project

--- Student PlannerTests/CoursesTests.swift
+++ Student PlannerTests/CoursesTests.swift
@@ -3,7 +3,7 @@ import XCTest
 
 class StudentPlannerTests: XCTestCase {
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        // Put setup code here. This method is called before the invocation of each test method in the class
     }
 
     override func tearDownWithError() throws {
