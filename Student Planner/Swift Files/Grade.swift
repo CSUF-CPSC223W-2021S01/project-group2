3334355336e46be6de8b53c4d4349639ca0d8210
Salam Abdelhalim <abed.pali@csu.fullerton.edu>
2021-05-13 23:00:56 -0700

    save

--- Student Planner/Swift Files/Grade.swift
+++ Student Planner/Swift Files/Grade.swift
@@ -1,11 +1,10 @@
 import Foundation
-class Grades {
+class Grades: Codable{
     var gradeRecieved: [String: Double] // dictionary with assignment name and grade recieved
     var gradePossible: [String: Double] // dictionary with assignment name and grade possible
     var recievedPoints: Double
     var possiblePoints: Double
     var grade: Double
-    var printGrade: String
    
     init?() {
         gradeRecieved = [:]
@@ -13,13 +12,11 @@ class Grades {
         recievedPoints = 0
         possiblePoints = 0
         grade = 0
-        printGrade = ""
     }
 
     func add(name: String, gradeRec: Double, gradePos: Double) {
         gradeRecieved[name] = gradeRec
         gradePossible[name] = gradePos
-    
     }
     
     func calcGrade() {
@@ -30,9 +27,25 @@ class Grades {
             possiblePoints += posValue
         }
         grade = round((recievedPoints/possiblePoints) * 100) * 100 / 100.0
-        
-        //printGrade = "\(grade)"
-        
+    }
+    
+    static let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let archiveURL = documentsDirectory.appendingPathComponent("Grade").appendingPathExtension("plist")
+
+
+    static func decodeGrades() -> [Grades]? {
+        guard let codedGrades = try? Data(contentsOf: archiveURL) else {
+            return nil
+        }
+        let propertyListDecoder = PropertyListDecoder()
+        return try? propertyListDecoder.decode([Grades].self, from: codedGrades)
+    }
+
+
+    static func saveGrades(_ grades: [Grades]) {
+        let propertyListEncoder = PropertyListEncoder()
+        let encodedCourses = try? propertyListEncoder.encode(grades)
+        try? encodedCourses?.write(to: archiveURL, options: .noFileProtection)
     }
 }
 
